d4df2678935db80ce260d0d1909f77960eb84e5f
sebastiansandoval27 <juan.sandovals@usantoto.edu.co>
2021-04-03 13:36:18 -0500

    Test fixed errors

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -5,12 +5,20 @@ const fetchWithoutToken = (endpoint, data, method = "GET") => {
   const url = `${base_url}/${endpoint}`;
 
   if (method === "GET") {
-    return fetch(url, { mode: "no-cors" });
+    return fetch(url, {
+      mode: "no-cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      },
+    });
   } else {
     return fetch(url, {
       method,
       headers: {
         "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       },
       mode: "no-cors",
       body: JSON.stringify(data),
@@ -31,6 +39,8 @@ const fetchWithToken = (endpoint, data, method = "GET") => {
         method,
         headers: {
           "x-token": token,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         },
         mode: "no-cors",
       });
@@ -40,6 +50,8 @@ const fetchWithToken = (endpoint, data, method = "GET") => {
         headers: {
           "Content-type": "application/json",
           "x-token": token,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         },
         mode: "no-cors",
         body: JSON.stringify(data),
